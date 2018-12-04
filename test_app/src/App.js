a4f4dd253b8ef9ba48a0e0cf3304737a743922bf
roman93ts <netsky93@gmail.com>
2018-12-05 01:10:51 +0300

    little adding in App

--- test_app/src/App.js
+++ test_app/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 	}
 
 	onField = (event) => {
-		const newValue = event.target.value;
+		const newValue = event.target.value.toLowerCase();
 		this.setState({
 			searchField: newValue
 		})
